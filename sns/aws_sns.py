db8f73511e93c1c07f7637285ef4844e5a7caa05
AndreiTVR <andreiteianu2000@gmail.com>
2024-11-20 17:42:10 +0200

    .

--- sns/aws_sns.py
+++ sns/aws_sns.py
@@ -1,7 +1,8 @@
+import boto3
 import sys
 import os
 import logging
-import boto3
+
 # For Local Use
 from botocore.exceptions import ClientError
 # for AWS Credentials, Uncomment for Use
@@ -21,19 +22,19 @@ class SNSManager:
         handler.setLevel(logging.INFO)
         self.logger.addHandler(handler)
 
-        if use_sns: # Comment if you want to use AWS Credentials
-            try:
-                self.sns_client = boto3.client('sns')
-                self.sns_resource = boto3.resource('sns')
-                self.topic_arn = load_aws_sns_topic_arn()
-            except (NoCredentialsError, PartialCredentialsError) as e:
-                self.logger.error("AWS credentials are not configured properly: %s", e)
-                raise
-        else:
-            self.logger.info("Running in local mode, SNS features disabled.")
-            self.sns_client = None
-            self.sns_resource = None
-            self.topic_arn = None
+        #  if use_sns: # Comment if you want to use AWS Credentials
+        #     try:
+        #         self.sns_client = boto3.client('sns')
+        #         self.sns_resource = boto3.resource('sns')
+        #         self.topic_arn = load_aws_sns_topic_arn()
+        #     except (NoCredentialsError, PartialCredentialsError) as e:
+        #         self.logger.error("AWS credentials are not configured properly: %s", e)
+        #         raise
+        # else:
+        #     self.logger.info("Running in local mode, SNS features disabled.")
+        #     self.sns_client = None
+        #     self.sns_resource = None
+        #     self.topic_arn = None
 
     
     def create_topic(self, name='BudgetAlerts'):
